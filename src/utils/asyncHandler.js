ff7c94611e4255e0d56d129752f9b15a38e64d3a
Vilas Balu Rabad <rabadvilas11@gmail.com>
2024-01-20 17:04:03 +0530

    Project Init 2

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+
+const asyncHandler = (fn) =>{
+   (req, res, next)=>{
+      Promises.resolve(fn(req, res, next))
+      .catch((err)=> next(err))
+   }
+}
+
+
+//OR
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//    try{
+//       await fn(req, res, next);
+//    }catch(error){
+//       res.status(error.code || 500).json({
+//          success: false,
+//          message: error.message
+//       })
+//    }
+// }
+
+
+export {asyncHandler};
